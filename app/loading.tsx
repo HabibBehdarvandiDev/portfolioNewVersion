19a45b3d3d385c0a66184d177b874cd61d866c4c
HabibBehdarvandiDev <habibbehdarvandi.dev@gmail.com>
2024-07-12 16:39:09 +0330

    create loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <main className='w-screen h-screen bg-background flex justify-center align-middle items-center absolute top-0 right-0 z-50'>
+        <div className='loader text-primary-600'>
+
+        </div>
+    </main>
+  )
+}
+
+
+export default loading
